7a6f41554516c34e69acb1cbe3da1a7da4d1f6c8
PRAVEENKUMAR <PRAVEENKUMAR@Praveen>
2018-02-16 16:19:15 -0700

    Updated code.

--- webservices/src/main/java/com/control2me/javaapi/SwaggerDocumentSetup.java
+++ webservices/src/main/java/com/control2me/javaapi/SwaggerDocumentSetup.java
@@ -1,5 +1,28 @@
 package com.control2me.javaapi;
 
-public class SwaggerDocumentSetup {
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
 
+import io.swagger.jaxrs.config.BeanConfig;
+
+public class SwaggerDocumentSetup extends HttpServlet
+{
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	public void init(ServletConfig config) throws ServletException
+	{
+		super.init(config);
+		
+		BeanConfig beanconfig = new BeanConfig();
+		beanconfig.setVersion("1.0.0");
+		beanconfig.setTitle("Sample REST APIs for testing purpose");
+		beanconfig.setDescription("For testing only");
+		beanconfig.setSchemes(new String[] {"http"});
+		beanconfig.setHost("localhost:8082");
+		beanconfig.setBasePath("/webservices/services");
+		beanconfig.setResourcePackage("com.control2me.javaapi.services");
+		beanconfig.setScan(true);
+	}
 }
